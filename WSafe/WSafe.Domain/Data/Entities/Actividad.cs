006860a6b462b737b927a95486023479dc337be5
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2021-11-22 13:31:06 -0500

    Configuración Tabla Riesgos

--- WSafe/WSafe.Domain/Data/Entities/Actividad.cs
+++ WSafe/WSafe.Domain/Data/Entities/Actividad.cs
@@ -1,8 +1,11 @@
-﻿namespace WSafe.Domain.Data.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Domain.Data.Entities
 {
     public class Actividad
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
         public string Descripcion { get; set; }
     }
 }
--- WSafe/WSafe.Domain/Data/Entities/CategoriaPeligro.cs
+++ WSafe/WSafe.Domain/Data/Entities/CategoriaPeligro.cs
@@ -1,14 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace WSafe.Domain.Data.Entities
 {
     public class CategoriaPeligro
     {
         public int ID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
         public string Descripcion { get; set; }
     }
 }
