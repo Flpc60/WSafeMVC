12eddc67ee582d1b9ae68bb60b5147f52e55363a
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2021-11-22 20:43:28 -0500

    Configuración tabla Riesgos

---
+++ WSafe/WSafe.Domain/Data/Entities/EfectosPosibles.cs
@@ -0,0 +1,9 @@
+﻿namespace WSafe.Domain.Data.Entities
+{
+    public enum EfectosPosibles
+    {
+        Daño_Leve = 1,
+        Daño_Medio = 2,
+        Daño_Extremo = 3
+    }
+}
---
+++ WSafe/WSafe.Domain/Data/Entities/NivelesExposicion.cs
@@ -0,0 +1,10 @@
+﻿namespace WSafe.Domain.Data.Entities
+{
+    public enum NivelesExposicion
+    {
+        Continua = 1,
+        Frecuente  = 2,
+        Ocasional = 3,
+        Esporadica = 4
+    }
+}
