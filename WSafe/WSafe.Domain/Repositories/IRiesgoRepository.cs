199ab4d10f35fd18099dd41df981adc14717d328
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2021-11-24 11:56:33 -0500

    Create Repositories, Services

---
+++ WSafe/WSafe.Domain/Repositories/IRiesgoRepository.cs
@@ -0,0 +1,8 @@
+﻿using WSafe.Domain.Data.Entities;
+
+namespace WSafe.Domain.Repositories
+{
+    public interface IRiesgoRepository : IGenericRepository<Riesgo>
+    {
+    }
+}
---
+++ WSafe/WSafe.Domain/Services/Implements/RiesgoService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSafe.Domain.Data.Entities;
+using WSafe.Domain.Repositories;
+
+namespace WSafe.Domain.Services.Implements
+{
+    public class RiesgoService : GenericService<Riesgo>, IRiesgoService
+    {
+        private IRiesgoRepository _riesgoRepository;
+
+        public RiesgoService(IRiesgoRepository riesgoRepository) : base(riesgoRepository)
+        {
+            _riesgoRepository = riesgoRepository;
+        }
+    }
+}
