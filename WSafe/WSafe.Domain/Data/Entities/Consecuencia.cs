905143ead68cd2e200428848697ae9ab702c75d6
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2021-11-23 19:33:53 -0500

    Creacion Initial Migration

--- WSafe/WSafe.Domain/Data/Entities/Consecuencia.cs
+++ WSafe/WSafe.Domain/Data/Entities/Consecuencia.cs
@@ -5,8 +5,8 @@ namespace WSafe.Domain.Data.Entities
     public class Consecuencia
     {
         public int ID { get; set; }
-        public int CategoriaPeligroID { get; set; }
-        public int PeligroID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        public CategoriaPeligro CategoriaPeligro { get; set; }
         public Peligro Peligro { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatotio")]
         public string Descripcion { get; set; }
--- WSafe/WSafe.Domain/Data/Entities/Peligro.cs
+++ WSafe/WSafe.Domain/Data/Entities/Peligro.cs
@@ -5,8 +5,8 @@ namespace WSafe.Domain.Data.Entities
     public class Peligro
     {
         public int ID { get; set; }
-        [Required(ErrorMessage = "El campo {0} es obligatorio")]
-        public int CategoriaPeligroID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        public CategoriaPeligro CategoriaPeligro { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         public string Descripcion { get; set; }
     }
