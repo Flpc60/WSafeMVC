727e981a3079fc0dec929b9c879ca892f11a95a6
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2021-11-24 16:07:57 -0500

    Migración Carga Inicial

---
+++ WSafe/WSafe.Domain/Migrations/202111241728205_UpdatePeligros.cs
@@ -0,0 +1,18 @@
+﻿namespace WSafe.Domain.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdatePeligros : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Peligroes", "CategoriaPeligroID", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Peligroes", "CategoriaPeligroID");
+        }
+    }
+}
--- WSafe/WSafe.Domain/Services/Implements/RiesgoService.cs
+++ WSafe/WSafe.Domain/Services/Implements/RiesgoService.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using WSafe.Domain.Data.Entities;
+﻿using WSafe.Domain.Data.Entities;
 using WSafe.Domain.Repositories;
 
 namespace WSafe.Domain.Services.Implements
