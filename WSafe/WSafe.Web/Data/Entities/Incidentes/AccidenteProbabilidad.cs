bc9e464976ec841c71fee975a1ff97d7d8e1b537
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2022-02-02 20:49:21 -0500

    implementación frontend incidentes

--- WSafe/WSafe.Web/Data/Entities/Incidentes/AccidenteProbabilidad.cs
+++ WSafe/WSafe.Web/Data/Entities/Incidentes/AccidenteProbabilidad.cs
@@ -5,14 +5,14 @@ namespace WSafe.Domain.Data.Entities.Incidentes
     public enum AccidenteProbabilidad
     {
         [Display(Name = "> 1 Año")]
-        Probabilidad1 =1,
+        A =1,
         [Display(Name = "1 Mes a 1 Año")]
-        Probabilidad2 = 2,
+        B = 2,
         [Display(Name = "Mensual")]
-        Probabilidad3 = 3,
+        C = 3,
         [Display(Name = "Semanal")]
-        Probabilidad4 = 4,
+        D = 4,
         [Display(Name = "Diario")]
-        Probabilidad5 = 5
+        E = 5
     }
 }
--- WSafe/WSafe.Web/Data/Entities/Incidentes/ConsecuenciasLesion.cs
+++ WSafe/WSafe.Web/Data/Entities/Incidentes/ConsecuenciasLesion.cs
@@ -4,13 +4,17 @@ namespace WSafe.Domain.Data.Entities.Incidentes
 {
     public enum ConsecuenciasLesion
     {
-        [Display(Name = "Leve")]
-        Leve = 1,
-        [Display(Name = "Grave")]
-        Grave = 2,
-        [Display(Name = "Muy grave")]
-        MuyGrave = 3,
-        [Display(Name = "Mortal")]
-        Mortal = 4
+        [Display(Name = "Sin Lesión: Casi accidente")]
+        sinLesion = 1,
+        [Display(Name = "Efecto Menor: Primer auxilio, lesión sin incapacidad")]
+        efectoMenor = 2,
+        [Display(Name = "Efecto Medio: Incap. parcial")]
+        efectoMedio = 3,
+        [Display(Name = "Efecto Mayor: Incap. permanent")]
+        EfectoMayor = 4,
+        [Display(Name = "Fatalidad e invalidez: Por ATEP")]
+        fatalidadInvalidez = 5,
+        [Display(Name = "Fatalidad Múltiple: Por ATEP")]
+        fatalidadMultiple = 6
     }
 }
