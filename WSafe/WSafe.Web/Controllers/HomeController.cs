e42b64824c8b3095c5d87ed52463ef4ad28a3721
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2022-03-14 20:21:33 -0500

    implementación accounts controller

--- WSafe/WSafe.Web/Controllers/HomeController.cs
+++ WSafe/WSafe.Web/Controllers/HomeController.cs
@@ -1,14 +1,16 @@
 ﻿using System.Web.Mvc;
+using WSafe.Web.Filters;
 
 namespace WSafe.Web.Controllers
 {
     public class HomeController : Controller
     {
+        [AuthorizeUser(roleID: 1, operationID: 1)]
         public ActionResult Index()
         {
             return View();
         }
-
+        [AuthorizeUser(roleID: 2, operationID: 2)]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -16,6 +18,7 @@ namespace WSafe.Web.Controllers
             return View();
         }
 
+        [AuthorizeUser(roleID: 3, operationID: 3)]
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
---
+++ WSafe/WSafe.Web/Models/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Web.Models
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        public int ID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "Correo electrónico")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+    }
+}
