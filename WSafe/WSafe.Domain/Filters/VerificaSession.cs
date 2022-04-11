65ac82a0b8871c6c8b7f8c81932ba28cfc83b25a
Francisco Luis Puerta Cardona <flpuertacardon@gmail.com>
2022-04-11 10:50:49 -0500

    implementación controlador acciones totalmente

---
+++ WSafe/WSafe.Domain/Filters/VerificaSession.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+using WSafe.Domain.Data.Entities;
+using WSafe.Web.Controllers;
+
+namespace WSafe.Web.Filters
+{
+    public class VerificaSession : System.Web.Mvc.ActionFilterAttribute
+    {
+        private User usuario;
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            try
+            {
+                base.OnActionExecuting(filterContext);
+                usuario = (User)HttpContext.Current.Session["User"];
+                if(usuario == null)
+                {
+                    if(filterContext.Controller is AccountsController == false)
+                    {
+                        filterContext.HttpContext.Response.Redirect("~/Accounts/Login");
+
+                    }
+                }
+
+            }
+            catch (Exception)
+            {
+                filterContext.Result = new RedirectResult("~/Accounts/Logout");
+            }
+        }
+
+    }
+}
