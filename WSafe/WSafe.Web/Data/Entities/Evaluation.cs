157224c0e076b33aba3fb61a51611ef65fd11c6c
Flpc60 <flpuertacardon@gmail.com>
2022-11-10 09:01:44 -0500

    implemented evaluations and sg-sst

--- WSafe/WSafe.Web/Data/Entities/Evaluation.cs
+++ WSafe/WSafe.Web/Data/Entities/Evaluation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 namespace WSafe.Domain.Data.Entities
@@ -7,11 +8,11 @@ namespace WSafe.Domain.Data.Entities
     {
         public int ID { get; set; }
         public int OrganizationID { get; set; }
-        public int NormaID { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatotio")]
         [Display(Name = "Fecha evaluación")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime FechaEvaluacion { get; set; }
+        public IEnumerable<Calification> Califications { get; set; }
     }
 }
---
+++ WSafe/WSafe.Web/Data/Entities/Movimient.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Domain.Data.Entities
+{
+    public class Movimient
+    {
+        public int ID { get; set; }
+        public int NormaID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "Nombre")]
+        [MaxLength(200)]
+        public string Name { get; set; }
+        public byte[]  Document { get; set; }
+    }
+}
