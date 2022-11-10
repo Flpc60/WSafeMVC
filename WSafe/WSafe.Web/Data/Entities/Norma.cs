cb2946cbd7bfd8e90fc2e07f1821e982d899ddb6
Flpc60 <flpuertacardon@gmail.com>
2022-11-09 19:40:30 -0500

    implemented standars evaluation

---
+++ WSafe/WSafe.Web/Data/Entities/Norma.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Domain.Data.Entities
+{
+    public class Norma
+    {
+        public int ID { get; set; }
+        public int OrganizationID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [MaxLength(2)]
+        public string Ciclo { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "Estándar")]
+        [MaxLength(200)]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "Valor del item")]
+        public decimal Valor { get; set; }
+    }
+}
