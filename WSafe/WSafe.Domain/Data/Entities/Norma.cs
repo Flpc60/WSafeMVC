98d7ae7d9fe4b4916076cf7b5387f76ff3e2edad
Flpc60 <flpuertacardon@gmail.com>
2022-12-19 18:50:40 -0500

    implemented evaluation controller calification actions

--- WSafe/WSafe.Domain/Data/Entities/Norma.cs
+++ WSafe/WSafe.Domain/Data/Entities/Norma.cs
@@ -7,17 +7,23 @@ namespace WSafe.Domain.Data.Entities
         public int ID { get; set; }
         public int OrganizationID { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "CICLO")]
         [MaxLength(2)]
         public string Ciclo { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "ITEM")]
         [MaxLength(6)]
         public string Item { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatotio")]
-        [Display(Name = "Estándar")]
+        [Display(Name = "NOMBRE")]
         [MaxLength(200)]
         public string Name { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatotio")]
-        [Display(Name = "Valor del item")]
+        [Display(Name = "VALOR")]
         public decimal Valor { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatotio")]
+        [Display(Name = "ESTÁNDAR")]
+        [MaxLength(2)]
+        public string Standard { get; set; }
     }
 }
