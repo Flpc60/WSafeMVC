52bf8d0c83784952f92a8ea9960cc90f19f9ac1b
Flpc60 <flpuertacardon@gmail.com>
2023-01-16 08:03:06 -0500

    implemented evaluations controller PlanActivities method ++

--- WSafe/WSafe.Web/App_Start/BundleConfig.cs
+++ WSafe/WSafe.Web/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace WSafe.Web
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/bootstrap").Include(
