21186cd25dd25f8896b874e4420bd8e43c030b50
Flpc60 <flpuertacardon@gmail.com>
2023-06-02 16:25:46 -0500

    Update remote DB

--- WSafe/WSafe.Web/Migrations/202306022123249_UpdIndicatorVM.cs
+++ WSafe/WSafe.Web/Migrations/202306022123249_UpdIndicatorVM.cs
@@ -3,7 +3,7 @@ namespace WSafe.Web.Migrations
     using System;
     using System.Data.Entity.Migrations;
     
-    public partial class CreateInitial : DbMigration
+    public partial class UpdIndicatorVM : DbMigration
     {
         public override void Up()
         {
