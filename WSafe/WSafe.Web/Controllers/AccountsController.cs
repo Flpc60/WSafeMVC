f01b6f9c69d31fc530e466e3e349671afe537b8b
Flpc60 <flpuertacardon@gmail.com>
2023-07-13 08:36:43 -0500

    implemented update Accounts Controller CreateRegister method

--- WSafe/WSafe.Web/Controllers/AccountsController.cs
+++ WSafe/WSafe.Web/Controllers/AccountsController.cs
@@ -208,6 +208,10 @@ namespace WSafe.Web.Controllers
                     var result = _empresaContext.Users.Where(u => u.Name == model.Name.Trim() && u.Email == model.Email.Trim() && u.Password == password.Trim()).FirstOrDefault();
                     if (result == null)
                     {
+                        if (model.ClientID == 0)
+                        { 
+                            model.ClientID = 1;
+                        }
                         model.Password = password;
                         _empresaContext.Users.Add(model);
                         await _empresaContext.SaveChangesAsync();
