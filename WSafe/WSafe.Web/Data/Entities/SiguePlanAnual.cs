20634613dd71b3e84ac3e95a5975154e6915e327
Flpc60 <flpuertacardon@gmail.com>
2023-11-22 19:44:42 -0500

    implemented anual planes controller

---
+++ WSafe/WSafe.Web/Data/Entities/SiguePlanAnual.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Domain.Data.Entities
+{
+    public class SiguePlanAnual
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "FECHA SEGUIMIENTO")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
+        public DateTime DateSigue { get; set; }
+        [Display(Name = "TRABAJADOR RESPONSABLE")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int TrabajadorID { get; set; }
+        public StatesActivity StateActivity { get; set; }
+        public StatesCronogram StateCronogram { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Range(typeof(short), "1", "9999", ErrorMessage = "Por favor ingrese un número de actividades ejecutadas válido.")]
+        public short Executed { get; set; }
+        [MaxLength(100)]
+        [Display(Name = "OBSERVACIONES")]
+        public string Observation { get; set; }
+    }
+}
