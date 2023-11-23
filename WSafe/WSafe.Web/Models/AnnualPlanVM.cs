e1de0566a9b60cf4b54f6020070aab56371cacc5
Flpc60 <flpuertacardon@gmail.com>
2023-11-23 07:08:07 -0500

    implemented AnnualPlans Controller

---
+++ WSafe/WSafe.Web/Models/AnnualPlanVM.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Web.Models
+{
+    public class AnnualPlanVM
+    {
+        public int ID { get; set; }
+        [Display(Name = "PHVA")]
+        [MaxLength(2)]
+        public string Cycle { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "ACTIVIDAD")]
+        [MaxLength(100)]
+        public string Activity { get; set; }
+        [MaxLength(100)]
+        [Display(Name = "ENTREGABLES")]
+        public string Entregables { get; set; }
+        [Display(Name = "RECURSOS")]
+        public string Recursos { get; set; }
+        [Display(Name = "TRABAJADOR RESPONSABLE")]
+        public string Responsable { get; set; }
+        [MaxLength(100)]
+        [Display(Name = "OBSERVACIONES")]
+        public string Observation { get; set; }
+        [Display(Name = "ESTADO")]
+        public string StateActivity { get; set; }
+        [Display(Name = "PROGRAMADAS")]
+        public short Programed { get; set; }
+        [Display(Name = "EJECUTADAS")]
+        public short Executed { get; set; }
+        [Display(Name = "% CUMPLIMIENT")]
+        public decimal PorcentajeCumplimiento { get; set; }
+    }
+}
--- WSafe/WSafe.Web/Models/SiguePlanAnualVM.cs
+++ WSafe/WSafe.Web/Models/SiguePlanAnualVM.cs
@@ -1,9 +1,12 @@
-﻿using System;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+using WSafe.Domain.Data.Entities;
+using System.Collections.Generic;
+using System.Web.Mvc;
 
-namespace WSafe.Domain.Data.Entities
+namespace WSafe.Web.Models
 {
-    public class SiguePlanAnual
+    public class SiguePlanAnualVM
     {
         public int ID { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
@@ -11,17 +14,20 @@ namespace WSafe.Domain.Data.Entities
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime DateSigue { get; set; }
-        [Display(Name = "TRABAJADOR RESPONSABLE")]
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Display(Name = "RESPONSABLE")]
         public int TrabajadorID { get; set; }
+        public IEnumerable<SelectListItem> Workers { get; set; }
+        [Display(Name = "ESTADO")]
         public StatesActivity StateActivity { get; set; }
+        [Display(Name = "CRONOGRAMA")]
         public StatesCronogram StateCronogram { get; set; }
         [Required(ErrorMessage = "El campo {0} es obligatorio")]
         [Range(typeof(short), "1", "9999", ErrorMessage = "Por favor ingrese un número de actividades ejecutadas válido.")]
+        [Display(Name = "TAREAS")]
         public short Executed { get; set; }
         [MaxLength(100)]
         [Display(Name = "OBSERVACIONES")]
         public string Observation { get; set; }
-        public int PlanActivityID { get; set; }
     }
 }
