f2e5ccc8560ccc7fda8aedff547847595a1f148b
Flpc60 <flpuertacardon@gmail.com>
2023-12-28 14:05:17 -0500

    Implemented Update SocidoDemographic Perfil

---
+++ WSafe/WSafe.Web/Migrations/202312281745145_UpdWorker.cs
@@ -0,0 +1,20 @@
+﻿namespace WSafe.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdWorker : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Trabajadors", "Profesion", c => c.String(maxLength: 50));
+            AlterColumn("dbo.WorkersVMs", "Profesion", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.WorkersVMs", "Profesion", c => c.String());
+            AlterColumn("dbo.Trabajadors", "Profesion", c => c.String());
+        }
+    }
+}
