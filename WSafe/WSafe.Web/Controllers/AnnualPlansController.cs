b6d3d27cbb5d4cf40c5f032798063280a5c87782
Flpc60 <flpuertacardon@gmail.com>
2023-12-28 17:24:21 -0500

    implemented WorkersController GenerateWorkersPdf Method

--- WSafe/WSafe.Web/Controllers/AnnualPlansController.cs
+++ WSafe/WSafe.Web/Controllers/AnnualPlansController.cs
@@ -528,6 +528,14 @@ namespace WSafe.Web.Controllers
                     ClientID = _clientID
                 };
                 _empresaContext.Movimientos.Add(movimient);
+
+                // Generar trazabilidad 
+                var model1 = _converterHelper.Traceability(organization.StandardAnnualPlan, year, _orgID, fullName);
+                if (model1 != null)
+                {
+                    _empresaContext.SigueAnnualPlans.Add(model1);
+                }
+
                 _empresaContext.SaveChanges();
                 return report;
             }
