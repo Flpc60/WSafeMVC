ebe024387d63e1a12e4ec1998ec2df18da4a5ae2
Flpc60 <flpuertacardon@gmail.com>
2024-02-01 17:36:54 -0500

    implemented Occupational Controller Index Method +++

---
+++ WSafe/WSafe.Web/Migrations/202402011810545_UpdOccupational2.cs
@@ -0,0 +1,30 @@
+﻿namespace WSafe.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdOccupational2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MedicalRecomendationVMs", "Document", c => c.String());
+            AddColumn("dbo.MedicalRecomendationVMs", "Age", c => c.String());
+            AddColumn("dbo.MedicalRecomendationVMs", "Cargo", c => c.String());
+            AlterColumn("dbo.MedicalRecomendationVMs", "Talla", c => c.String());
+            AlterColumn("dbo.MedicalRecomendationVMs", "Peso", c => c.String());
+            AlterColumn("dbo.Occupationals", "Talla", c => c.Short(nullable: false));
+            AlterColumn("dbo.Occupationals", "Peso", c => c.Short(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Occupationals", "Peso", c => c.Int(nullable: false));
+            AlterColumn("dbo.Occupationals", "Talla", c => c.Int(nullable: false));
+            AlterColumn("dbo.MedicalRecomendationVMs", "Peso", c => c.Int(nullable: false));
+            AlterColumn("dbo.MedicalRecomendationVMs", "Talla", c => c.Int(nullable: false));
+            DropColumn("dbo.MedicalRecomendationVMs", "Cargo");
+            DropColumn("dbo.MedicalRecomendationVMs", "Age");
+            DropColumn("dbo.MedicalRecomendationVMs", "Document");
+        }
+    }
+}
