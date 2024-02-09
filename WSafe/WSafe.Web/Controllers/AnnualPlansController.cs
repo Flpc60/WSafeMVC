01153c6a96afc6babbdb0813c26f8c8bb44eabfa
Flpc60 <flpuertacardon@gmail.com>
2024-02-09 16:49:00 -0500

    implemented Occupationals Controller Create Method

--- WSafe/WSafe.Web/Controllers/AnnualPlansController.cs
+++ WSafe/WSafe.Web/Controllers/AnnualPlansController.cs
@@ -18,7 +18,7 @@ namespace WSafe.Web.Controllers
 {
     public class AnnualPlansController : Controller
     {
-        //  Plan de trabajo anual en el SG-SST
+        //  Plan de trabajo anual en el SG-SST...
         private int _clientID;
         private int _orgID;
         private string _year;
---
+++ WSafe/WSafe.Web/Migrations/202402092059181_UpdOrganization.cs
@@ -0,0 +1,46 @@
+﻿namespace WSafe.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdOrganization : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.CreateOccupationalVMs",
+                c => new
+                    {
+                        ID = c.Int(nullable: false, identity: true),
+                        ExaminationDate = c.DateTime(nullable: false),
+                        TrabajadorID = c.Int(nullable: false),
+                        Talla = c.Short(nullable: false),
+                        Peso = c.Short(nullable: false),
+                        ExaminationType = c.Int(nullable: false),
+                        Recomendations = c.String(maxLength: 500),
+                        MedicalRecomendation = c.Int(nullable: false),
+                        OrganizationID = c.Int(nullable: false),
+                        ClientID = c.Int(nullable: false),
+                        UserID = c.Int(nullable: false),
+                        FileName = c.String(nullable: false, maxLength: 200),
+                    })
+                .PrimaryKey(t => t.ID);
+            
+            AddColumn("dbo.Occupationals", "FileName", c => c.String(nullable: false, maxLength: 200));
+            AddColumn("dbo.SigueOccupationals", "CreateOccupationalVM_ID", c => c.Int());
+            AddColumn("dbo.Organizations", "StandardOccupational", c => c.Short(nullable: false));
+            CreateIndex("dbo.SigueOccupationals", "CreateOccupationalVM_ID");
+            AddForeignKey("dbo.SigueOccupationals", "CreateOccupationalVM_ID", "dbo.CreateOccupationalVMs", "ID");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.SigueOccupationals", "CreateOccupationalVM_ID", "dbo.CreateOccupationalVMs");
+            DropIndex("dbo.SigueOccupationals", new[] { "CreateOccupationalVM_ID" });
+            DropColumn("dbo.Organizations", "StandardOccupational");
+            DropColumn("dbo.SigueOccupationals", "CreateOccupationalVM_ID");
+            DropColumn("dbo.Occupationals", "FileName");
+            DropTable("dbo.CreateOccupationalVMs");
+        }
+    }
+}
