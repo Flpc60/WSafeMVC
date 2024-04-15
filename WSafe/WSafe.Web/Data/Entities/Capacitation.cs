62f0451cd8c00fb5f55067f83fb59f9d35e4e9f2
Flpc60 <flpuertacardon@gmail.com>
2024-04-15 16:36:44 -0500

    implemented capacitation create data base

---
+++ WSafe/WSafe.Web/Data/Entities/Capacitation.cs
@@ -0,0 +1,53 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Domain.Data.Entities
+{
+    public class Capacitation
+    {
+        public int ID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime InitialDate { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime EndDate { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(200)]
+        public string Activity { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(200)]
+        public int Objetive { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MaxLength(200)]
+        public string Content { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public int TrabajadorID { get; set; }
+        public StatesCronogram StateCronogram { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Range(typeof(short), "1", "9999", ErrorMessage = "Por favor ingrese un número de actividades programadas válido.")]
+        public short Programed { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Range(typeof(short), "1", "9999", ErrorMessage = "Por favor ingrese un número de trabajadores citados  válido.")]
+        public short Citados { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Range(typeof(short), "1", "9999", ErrorMessage = "Por favor ingrese un número de trabajadores capacitados válido.")]
+        public short Capacitados { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [Range(typeof(short), "1", "9999", ErrorMessage = "Por favor ingrese un número de trabajadores evaluados válido.")]
+        public short Evaluados { get; set; }
+        [MaxLength(200)]
+        public string Observation { get; set; }
+        public ActionCategories ActionCategory { get; set; }
+        public int OrganizationID { get; set; }
+        public int ClientID { get; set; }
+        public int UserID { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public ActivitiesFrequency ActivityFrequency { get; set; }
+        public ICollection<Schedule> Shedule { get; set; }
+    }
+}
