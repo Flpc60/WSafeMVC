811450185bc426d4787b8080484d79b76c04b29e
Flpc60 <flpuertacardon@gmail.com>
2024-07-20 19:17:06 -0500

    implemented AddControlTrace

---
+++ WSafe/WSafe.Web/Migrations/202407210015421_CrtControlTrace.cs
@@ -0,0 +1,22 @@
+﻿namespace WSafe.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CrtControlTrace : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.MainCauses", "OrganizationID", c => c.Int(nullable: false));
+            AddColumn("dbo.MainCauses", "ClientID", c => c.Int(nullable: false));
+            AddColumn("dbo.MainCauses", "UserID", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.MainCauses", "UserID");
+            DropColumn("dbo.MainCauses", "ClientID");
+            DropColumn("dbo.MainCauses", "OrganizationID");
+        }
+    }
+}
