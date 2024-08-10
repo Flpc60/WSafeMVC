09aaac4b9869f23d8d726789112690ffff1f6dc4
Flpc60 <flpuertacardon@gmail.com>
2024-08-10 17:40:00 -0500

    update table Accion

---
+++ WSafe/WSafe.Web/Data/Entities/NonConformity.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Domain.Data.Entities
+{
+    public class NonConformity
+    {
+        public int ID { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+    }
+}
