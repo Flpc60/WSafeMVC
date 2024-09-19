c30303e074558b1cf2045d3cdd3aa9b1fecba7f2
Flpc60 <flpuertacardon@gmail.com>
2024-09-19 18:12:20 -0500

    include tables for EmergenciesController

---
+++ WSafe/WSafe.Web/Data/Entities/Ppre/ScalesCalification.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WSafe.Domain.Data.Entities.Ppre
+{
+    public enum ScalesCalification
+    {
+        Sí = 0,
+        Parcial = 1,
+        No = 2
+    }
+    public static class ScalesCalificationValues
+    {
+        public static readonly Dictionary<ScalesCalification, double> Values = new Dictionary<ScalesCalification, double>
+        {
+            { ScalesCalification.Sí, 0.0 },
+            { ScalesCalification.Parcial, 0.5 },
+            { ScalesCalification.No, 1.0 }
+        };
+    }
+}
---
+++ WSafe/WSafe.Web/Models/NivelRiesgoVM.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WSafe.Web.Models
+{
+    public class NivelRiesgoVM
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Display(Name = "Clase de Amenaza")]
+        public string CategoryAmenaza { get; set; }
+
+        [Display(Name = "Amenaza")]
+        public string Amenaza { get; set; }
+
+        [Display(Name = "Respuesta")]
+        public string Calification { get; set; }
+        [Display(Name = "Personas(P)")]
+        public string Personas { get; set; }
+        [Display(Name = "Recursos(R)")]
+        public string Resursos { get; set; }
+        [Display(Name = "Sistemas y procesps(S)")]
+        public string Sistemas { get; set; }
+    }
+}
